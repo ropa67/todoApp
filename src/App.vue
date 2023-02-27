97f281464c0996c7b3ca6bf9745b3a3630809c11
kropski <kropski@divante.pl>
2023-02-27 08:34:14 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,129 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<main class="app">
+		
+		<section class="greeting">
+			<h2 class="title">
+				What's up, <input type="text" id="name" placeholder="Name here" v-model="name">
+			</h2>
+		</section>
+
+		<section class="create-todo">
+			<h3>CREATE A TODO</h3>
+
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>What's on your todo list?</h4>
+				<input 
+					type="text" 
+					name="content" 
+					id="content" 
+					placeholder="e.g. make a video"
+					v-model="input_content" />
+				
+				<h4>Pick a category</h4>
+				<div class="options">
+
+					<label>
+						<input 
+							type="radio" 
+							name="category" 
+							id="category1" 
+							value="business"
+							v-model="input_category" />
+						<span class="bubble business"></span>
+						<div>Business</div>
+					</label>
+
+					<label>
+						<input
+							type="radio" 
+							name="category" 
+							id="category2" 
+							value="personal"
+							v-model="input_category" />
+						<span class="bubble personal"></span>
+						<div>Personal</div>
+					</label>
+
+				</div>
+
+				<input type="submit" value="Add todo" />
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<h3>TODO LIST</h3>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_asc" :key="todo.id" :class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="todo.done" />
+						<span :class="`bubble ${
+							todo.category == 'business'
+								? 'business'
+								: 'personal'
+						}`"></span>
+					</label>
+
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" />
+					</div>
+
+					<div class="actions">
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+// eslint-disable-next-line no-unused-vars
+import {ref, onMounted, computed, watch} from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const input_content = ref('')
+const todos = ref([])
+const input_category = ref(null)
+const name = ref('')
+
+ // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+ const todos_asc = computed(() => todos.value.sort((a, b) => {
+   return b.createdAt - a.createdAt
+ }))
+
+function addTodo() {
+  if(input_content.value.trim()===''||input_category.value===null) {
+    return
   }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    //  createdAt: new Data().getItem()
+  })
+
+  input_content.value = ''
+  input_category.value = null
+
+}
+
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, {deep: true})
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos') || '[]')
+
+})
+
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
